680ab85dc2586c1c6c289fc098c79adf7386b037
Marcel Klehr <mklehr@gmx.net>
2021-07-29 17:51:28 +0200

    Classifier tests: Test with negatives for FNR

--- test/tests.js
+++ test/tests.js
@@ -5,93 +5,127 @@ const YAML = require('yaml')
 const flatten = require('lodash/flatten')
 const uniq = require('lodash/uniq')
 const execa = require('execa')
-const glob = require('fast-glob');
+const glob = require('fast-glob')
 const Parallel = require('async-parallel')
 
 const rules = YAML.parse(fsSync.readFileSync(__dirname + '/../src/rules.yml').toString('utf8'))
 
 const PHOTOS_PER_LABEL = 100
 const PHOTOS_OLDER_THAN = 1627464319 // 2021-07-28; for determinism
-const flickr = new Flickr(process.env.FLICKR_API_KEY);
+const flickr = new Flickr(process.env.FLICKR_API_KEY)
 
 const labels = uniq(flatten(Object.entries(rules)
-    .map(([key, entry]) =>
-        entry.label ?
-            [entry.label]
-            : []
-    )))
+	.map(([key, entry]) =>
+		entry.label
+			? [entry.label]
+			: []
+	)))
 
 ;(async function() {
-    const results = await Parallel.map(labels, async label => {
-        try {
-            const urls = await findPhotos(label)
-            await Promise.all(
-                urls.map(url => download(url, 'temp_images/' + label))
-            )
-        }catch(e) {
+	const results = await Parallel.map(labels, async label => {
+		// First we calculate the true positive rate
+        let tpr
+
+		try {
+			const urls = await findPhotos(label)
+			await Promise.all(
+				urls.map(url => download(url, 'temp_images/' + label))
+			)
+
+			const files = await glob(['temp_images/' + label + '/*'])
+			if (!files.length) {
+				console.log('No photos found for label "' + label + '"')
+				return 0
+			}
+			const { stdout } = await execa('node', [__dirname + '/../src/classifier.js'].concat(files))
+			const predictions = stdout.split('\n')
+				.map(line => JSON.parse(line))
+			const matches = predictions
+				.map((labels, i) => labels.includes(label) ? files[i] : labels)
+
+			const matchCount = matches.filter((item, i) => files[i] === item).length
+			tpr = matchCount / files.length
+
+            console.log('Processed photos for label "' + label + '"')
+
+
+
+        let tnr = 0
+
+		if (rules[label].categories) {
+			// If we have categories, we calculate false positive rate
+				for (const category of rules[label].categories) {
+					const urls = await findPhotos(category + ' -' + label)
+					await Promise.all(
+						urls.map(url => download(url, 'temp_images/-' + label))
+					)
+				}
+
+				const files = await glob(['temp_images/-' + label + '/*'])
+				if (!files.length) {
+					console.log('No photos found for label -"' + label + '"')
+					return 0
+				}
+				const { stdout } = await execa('node', [__dirname + '/../src/classifier.js'].concat(files))
+				const predictions = stdout.split('\n')
+					.map(line => JSON.parse(line))
+				const matches = predictions
+					.map((labels, i) => labels.includes(label) ? files[i] : labels)
+
+				const matchCount = matches.filter((item, i) => files[i] === item).length
+				tnr = 1- (matchCount / (files.length))
+
+                console.log('Processed photos for label "' + label + '"')
+		}
+
+        } catch (e) {
             console.log(e)
-            return
         }
 
-        const files = await glob(['temp_images/'+label+'/*'])
-        if (!files.length) {
-            console.log('No photos found for label "'+label+'"')
-            return 0
-        }
-        const {stdout} = await execa('node', [__dirname+'/../src/classifier.js'].concat(files))
-        const predictions = stdout.split('\n')
-            .map(line => JSON.parse(line))
-        const matches = predictions
-            .map((labels, i) => labels.includes(label)? files[i] : labels)
+		console.log({tpr, tnr})
 
-        const matchCount = matches.filter((item, i) => files[i] === item).length
-        const missCount = files.length - matchCount
-        const misses = matches
-            .map((item) => typeof item === 'string'? null : item)
-            .map((labels, i) => labels? [files[i], labels] : null)
-            .filter(Boolean)
+		return {tpr, tnr}
+	}, 20)
 
-        console.log('Processed photos for label "'+label+'"')
-        console.log(matchCount/PHOTOS_PER_LABEL)
+	const sum = results.reduce((acc, val) => {
+		return {tpr: acc.tpr+val.tpr, tnr: acc.tnr+val.tnr}
+	}, {tpr:0, tnr:0})
 
-        return matchCount/PHOTOS_PER_LABEL
-    }, 20)
+    const averageTPR = sum.tpr/results.length
+    const averageTNR = sum.tnr/results.length
+    const balancedAccuracy =  (averageTPR+averageTNR)/2
 
-    const averageMatchRate = results.reduce((acc, val) => {
-        return acc+val
-    }, 0) / results.length
+	console.log({ averageTPR, averageTNR, balancedAccuracy})
 
-    console.log({averageMatchRate})
+	const worstLabels = Object.fromEntries(
+		results
+			.map((result, i) => [labels[i], result])
+			.filter(([, result]) => (result.tpr+result.tnr) < balancedAccuracy)
+	)
 
-    const worstLabels = Object.fromEntries(
-        results
-            .map((rate, i) => [labels[i], rate])
-            .filter(([, rate]) => rate < averageMatchRate)
-    )
+	console.log({ worstLabels })
 
-    console.log({worstLabels})
-
-    if (averageMatchRate < 0.5) {
-        process.exit(1)
-    }
+	if (balancedAccuracy < 0.5) {
+		process.exit(1)
+	}
 })()
 
 function findPhotos(label) {
-    return flickr.photos.search({
-        text: label,
-        per_page: PHOTOS_PER_LABEL,
-        media: 'photos',
-        content_type: 1,
-        max_upload_date: PHOTOS_OLDER_THAN,
-        sort: 'relevance',
-    }).then(function (res) {
-        if (res.body.stat === 'ok') {
-            return res.body.photos.photo.map(photo => `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`)
-        }else{
-            console.log(res.body)
-        }
+	return flickr.photos.search({
+		text: label,
+		per_page: PHOTOS_PER_LABEL,
+		media: 'photos',
+		content_type: 1,
+		max_upload_date: PHOTOS_OLDER_THAN,
+		sort: 'relevance',
+	}).then(function(res) {
+		if (res.body.stat === 'ok') {
+			return res.body.photos.photo.map(photo => `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`)
+		} else {
+			console.log(res.body)
+		}
 
-    }).catch(function (err) {
-        throw err
-    });
+	}).catch(function(err) {
+		throw err
+	})
 }
