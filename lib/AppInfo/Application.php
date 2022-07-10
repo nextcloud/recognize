9baa524d65b54c17a66ce4103b6afa0f4f88c7ff
Marcel Klehr <mklehr@gmx.net>
2022-07-10 14:39:37 +0200

    Handle file deletions
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -8,22 +8,32 @@
 
 namespace OCA\Recognize\AppInfo;
 
+use OCA\Recognize\Hooks\FileListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\EventDispatcher\IEventDispatcher;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'recognize';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);
-	}
+        /**
+         * @var IEventDispatcher $dispatcher
+         */
+        $dispatcher = $this->getContainer()->get(IEventDispatcher::class);
+        $dispatcher->addServiceListener('\OCP\Files::postDelete', FileListener::class);
+    }
 
 	public function register(IRegistrationContext $context): void {
         @include_once __DIR__ . '/../../vendor/autoload.php';
 	}
 
-	public function boot(IBootContext $context): void {
+    /**
+     * @throws \Throwable
+     */
+    public function boot(IBootContext $context): void {
 	}
 }
