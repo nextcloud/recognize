512be093d6a35f32b6ec4bf96c7b227d237b7230
Marcel Klehr <mklehr@gmx.net>
2022-07-13 14:18:54 +0200

    Build queue tables in DB for performance
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

---
+++ lib/Db/Video.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OCA\Recognize\Db;
+
+use OCP\AppFramework\Db\Entity;
+
+/**
+ * Class FaceCluster
+ *
+ * @package OCA\Recognize\Db
+ * @method string getFileId()
+ * @method setFileId(int $fileId)
+ * @method string getUserId()
+ * @method setUserId(string $userId)
+ * @method setProcessedMovinet(boolean $processed)
+ * @method bool getProcessedMovinet()
+ */
+class Video extends Entity {
+    public $id;
+	protected int $fileId;
+	protected string $userId;
+	protected bool $processedMovinet;
+
+	public static array $columns = ['id', 'file_id', 'user_id', 'processed_movinet'];
+	public static array $fields = ['id', 'fileId', 'userId', 'processedMovinet'];
+
+	public function __construct() {
+		// add types in constructor
+		$this->addType('id', 'integer');
+		$this->addType('file_id', 'integer');
+		$this->addType('userId', 'string');
+		$this->addType('processedMovinet', 'boolean');
+	}
+
+	public function toArray(): array {
+		$array = [];
+		foreach (self::$fields as $field) {
+			$array[$field] = $this->{$field};
+		}
+		return $array;
+	}
+}
---
+++ lib/Migration/Version002003000Date20220713094721.php
@@ -0,0 +1,119 @@
+<?php
+/*
+ * Copyright (c) 2020. The Nextcloud Bookmarks contributors.
+ *
+ * This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
+ */
+
+namespace OCA\Recognize\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version002003000Date20220713094721 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return void
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if (!$schema->hasTable('recognize_files_images')) {
+			$table = $schema->createTable('recognize_files_images');
+			$table->addColumn('file_id', 'bigint', [
+				'notnull' => true,
+				'length' => 64,
+			]);
+			$table->addColumn('user_id', 'string', [
+				'notnull' => false,
+				'length' => 64,
+			]);
+            $table->addColumn('processed_geo', 'boolean', [
+                'notnull' => true,
+            ]);
+            $table->addColumn('processed_imagenet', 'boolean', [
+                'notnull' => true,
+            ]);
+            $table->addColumn('processed_landmarks', 'boolean', [
+                'notnull' => true,
+            ]);
+            $table->addColumn('processed_faces', 'boolean', [
+                'notnull' => true,
+            ]);
+			$table->setPrimaryKey(['file_id'], 'recognize_images_id');
+			$table->addIndex(['user_id'], 'recognize_images_user');
+			$table->addIndex(['user_id', 'processed_imagenet'], 'recognize_imagenet_user');
+			$table->addIndex(['user_id', 'processed_landmarks'], 'recognize_landmarks_user');
+			$table->addIndex(['user_id', 'processed_faces'], 'recognize_faces_user');
+			$table->addIndex(['user_id', 'processed_geo'], 'recognize_faces_user');
+		}
+
+        if (!$schema->hasTable('recognize_files_video')) {
+            $table = $schema->createTable('recognize_files_video');
+            $table->addColumn('file_id', 'bigint', [
+                'notnull' => true,
+                'length' => 64,
+            ]);
+            $table->addColumn('user_id', 'string', [
+                'notnull' => false,
+                'length' => 64,
+            ]);
+            $table->addColumn('processed_movinet', 'boolean', [
+                'notnull' => true,
+            ]);
+            $table->setPrimaryKey(['file_id'], 'recognize_video_id');
+            $table->addIndex(['user_id'], 'recognize_video_user');
+            $table->addIndex(['user_id', 'processed_movinet'], 'recognize_movinet_user');
+        }
+
+        if (!$schema->hasTable('recognize_files_audio')) {
+            $table = $schema->createTable('recognize_files_audio');
+            $table->addColumn('file_id', 'bigint', [
+                'notnull' => true,
+                'length' => 64,
+            ]);
+            $table->addColumn('user_id', 'string', [
+                'notnull' => false,
+                'length' => 64,
+            ]);
+            $table->addColumn('processed_musicnn', 'boolean', [
+                'notnull' => true,
+            ]);
+            $table->setPrimaryKey(['file_id'], 'recognize_audio_id');
+            $table->addIndex(['user_id'], 'recognize_audio_user');
+            $table->addIndex(['user_id', 'processed_musicnn'], 'recognize_musicnn_user');
+        }
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return void
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
