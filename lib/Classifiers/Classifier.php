895165a208eb9cb4576c2eb8f73d8b60e8632361
Marcel Klehr <mklehr@gmx.net>
2022-07-15 13:54:22 +0200

    Classifier.php: Re-add copyright
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/Classifiers/Classifier.php
+++ lib/Classifiers/Classifier.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Copyright (c) 2021. The Nextcloud Recognize contributors.
+ *
+ * This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
+ */
 
 namespace OCA\Recognize\Classifiers;
 
