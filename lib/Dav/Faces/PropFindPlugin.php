e489defae082cceef192eaab88caace2efea4e1f
Marcel Klehr <mklehr@gmx.net>
2022-08-19 10:33:15 +0200

    PropFindPlugin: Use existing constants
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/Dav/Faces/PropFindPlugin.php
+++ lib/Dav/Faces/PropFindPlugin.php
@@ -3,7 +3,8 @@
 namespace OCA\Recognize\Dav\Faces;
 
 use OCA\DAV\Connector\Sabre\File;
-use OCA\Recognize\Db\FaceClusterMapper;
+use OCA\DAV\Connector\Sabre\FilesPlugin;
+use OCA\DAV\Connector\Sabre\TagsPlugin;
 use OCA\Recognize\Db\FaceDetection;
 use OCA\Recognize\Db\FaceDetectionMapper;
 use OCA\Recognize\Db\FaceDetectionWithTitle;
@@ -13,18 +14,15 @@ use Sabre\DAV\Server;
 use Sabre\DAV\ServerPlugin;
 
 class PropFindPlugin extends ServerPlugin {
+	public const FACE_DETECTIONS_PROPERTYNAME = '{http://nextcloud.org/ns}face-detections';
+	public const FILE_NAME_PROPERTYNAME = '{http://nextcloud.org/ns}file-name';
+	public const REALPATH_PROPERTYNAME = '{http://nextcloud.org/ns}realpath';
+
 	private Server $server;
 	private FaceDetectionMapper $faceDetectionMapper;
-	private FaceClusterMapper $clusterMapper;
 
-	public const INTERNAL_FILEID_PROPERTYNAME = '{http://owncloud.org/ns}fileid';
-	public const FILE_METADATA_SIZE = '{http://nextcloud.org/ns}file-metadata-size';
-	public const HAS_PREVIEW_PROPERTYNAME = '{http://nextcloud.org/ns}has-preview';
-	public const FAVORITE_PROPERTYNAME = '{http://owncloud.org/ns}favorite';
-
-	public function __construct(FaceDetectionMapper $faceDetectionMapper, FaceClusterMapper $clusterMapper) {
+	public function __construct(FaceDetectionMapper $faceDetectionMapper) {
 		$this->faceDetectionMapper = $faceDetectionMapper;
-		$this->clusterMapper = $clusterMapper;
 	}
 
 	public function initialize(Server $server) {
@@ -36,24 +34,24 @@ class PropFindPlugin extends ServerPlugin {
 
 	public function propFind(PropFind $propFind, INode $node) {
 		if ($node instanceof FacePhoto) {
-			$propFind->handle('{http://nextcloud.org/ns}file-name', function () use ($node) {
+			$propFind->handle(self::FILE_NAME_PROPERTYNAME, function () use ($node) {
 				return $node->getFile()->getName();
 			});
 
-			$propFind->handle('{http://nextcloud.org/ns}face-detections', function () use ($node) {
+			$propFind->handle(self::FACE_DETECTIONS_PROPERTYNAME, function () use ($node) {
 				return json_encode(array_map(fn (FaceDetectionWithTitle $face) => $face->toArray(), array_values(array_filter($this->faceDetectionMapper->findByFileIdWithTitle($node->getFile()->getId()), fn (FaceDetection $face) => $face->getClusterId() !== null))));
 			});
 
-			$propFind->handle(self::INTERNAL_FILEID_PROPERTYNAME, fn () => $node->getFile()->getId());
-			$propFind->handle('{http://nextcloud.org/ns}file-name', fn () => $node->getFile()->getName());
-			$propFind->handle('{http://nextcloud.org/ns}realpath', fn () => $node->getFile()->getPath());
-			$propFind->handle(self::FILE_METADATA_SIZE, fn () => json_encode($node->getMetadata()));
-			$propFind->handle(self::HAS_PREVIEW_PROPERTYNAME, fn () => json_encode($node->hasPreview()));
-			$propFind->handle(self::FAVORITE_PROPERTYNAME, fn () => $node->isFavorite() ? 1 : 0);
+			$propFind->handle(FilesPlugin::INTERNAL_FILEID_PROPERTYNAME, fn () => $node->getFile()->getId());
+			$propFind->handle(self::FILE_NAME_PROPERTYNAME, fn () => $node->getFile()->getName());
+			$propFind->handle(self::REALPATH_PROPERTYNAME, fn () => $node->getFile()->getPath());
+			$propFind->handle(FilesPlugin::FILE_METADATA_SIZE, fn () => json_encode($node->getMetadata()));
+			$propFind->handle(FilesPlugin::HAS_PREVIEW_PROPERTYNAME, fn () => json_encode($node->hasPreview()));
+			$propFind->handle(TagsPlugin::FAVORITE_PROPERTYNAME, fn () => $node->isFavorite() ? 1 : 0);
 		}
 
 		if ($node instanceof File) {
-			$propFind->handle('{http://nextcloud.org/ns}face-detections', function () use ($node) {
+			$propFind->handle(self::FACE_DETECTIONS_PROPERTYNAME, function () use ($node) {
 				return json_encode(array_map(fn (FaceDetectionWithTitle $face) => $face->toArray(), array_values(array_filter($this->faceDetectionMapper->findByFileIdWithTitle($node->getId()), fn (FaceDetection $face) => $face->getClusterId() !== null))));
 			});
 		}
