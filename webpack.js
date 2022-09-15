339f8f1b29fff5bac03df727e3db4aa9658b620a
Marcel Klehr <mklehr@gmx.net>
2022-09-15 11:12:03 +0200

    Remove user settings
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- webpack.js
+++ webpack.js
@@ -3,4 +3,3 @@ const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 module.exports = webpackConfig
 webpackConfig.entry.admin = path.join(__dirname, 'src', 'admin.js')
-webpackConfig.entry.user = path.join(__dirname, 'src', 'user.js')
