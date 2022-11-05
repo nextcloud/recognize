aa225bffe7d6288ba986005d2e789bdb3d01bf4a
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-11-05 16:35:46 +0900

    add formats
    Signed-off-by: ふぁ <34892635+fa0311@users.noreply.github.com>

--- lib/Constants.php
+++ lib/Constants.php
@@ -6,5 +6,7 @@ class Constants {
 	public const IMAGE_FORMATS = ['image/jpeg', 'image/png', 'image/bmp', 'image/heic', 'image/heif', 'image/tiff'];
 	public const AUDIO_FORMATS = ['audio/mpeg', 'audio/mp4', 'audio/ogg', 'audio/vnd.wav', 'audio/flac'];
 	public const VIDEO_FORMATS = ['image/gif', 'video/mp4', 'video/MP2T', 'video/x-msvideo', 'video/x-ms-wmv', 'video/quicktime', 'video/ogg', 'video/mpeg', 'video/webm', 'video/x-matroska'];
+	public const DIRECTORY_FORMATS = ['httpd/unix-directory'];
+	public const OCTET_STREAM_FORMATS = ['application/octet-stream'];
 	public const MAX_FILE_SIZE = 10000000;
 }
