867dd08511270d105880b737d26a35e35ad7fec8
Marcel Klehr <mklehr@gmx.net>
2022-11-15 14:34:05 +0100

    Fix psalm errors
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/Service/IgnoreService.php
+++ lib/Service/IgnoreService.php
@@ -23,36 +23,53 @@ class IgnoreService {
 		$this->mimeTypes = $mimeTypes;
 	}
 
+	/**
+	 * @param int $fileid
+	 * @param array $directoryTypes
+	 * @param bool $recursive
+	 * @return list<array>
+	 */
 	public function getDir(int $fileid, array $directoryTypes, bool $recursive = false): array {
-		/** @var \OCP\DB\QueryBuilder\IQueryBuilder $qb */
 		$qb = new CacheQueryBuilder($this->db, $this->systemConfig, $this->logger);
-		$dir = $qb->selectFileCache()
+		$result = $qb->selectFileCache()
 			->andWhere($qb->expr()->in('mimetype', $qb->createNamedParameter($directoryTypes, IQueryBuilder::PARAM_INT_ARRAY)))
 			->andWhere($qb->expr()->eq('parent', $qb->createNamedParameter($fileid)))
-			->executeQuery()->fetchAll();
+			->executeQuery();
+		/**
+		 * @var list<array> $dir
+		 */
+		$dir = $result->fetchAll();
 
 		if ($recursive) {
 			foreach ($dir as $item) {
-				$dir = array_merge($dir, $this->getDir($item['fileid'], $directoryTypes, $recursive));
+				$dir = array_merge($dir, $this->getDir((int) $item['fileid'], $directoryTypes, $recursive));
 			}
 		}
 		return $dir;
 	}
 
+	/**
+	 * @param int $storageId
+	 * @param array $ignoreMarkers
+	 * @return array
+	 */
 	public function getIgnoredDirectories(int $storageId, array $ignoreMarkers): array {
 		$directoryTypes = array_map(fn ($mimeType) => $this->mimeTypes->getId($mimeType), Constants::DIRECTORY_FORMATS);
-		/** @var \OCP\DB\QueryBuilder\IQueryBuilder $qb */
 		$qb = new CacheQueryBuilder($this->db, $this->systemConfig, $this->logger);
-		$ignoreFiles = $qb->selectFileCache()
+		$result = $qb->selectFileCache()
 			->andWhere($qb->expr()->in('name', $qb->createNamedParameter($ignoreMarkers, IQueryBuilder::PARAM_STR_ARRAY)))
 			->andWhere($qb->expr()->eq('storage', $qb->createNamedParameter($storageId, IQueryBuilder::PARAM_INT)))
-			->executeQuery()->fetchAll();
-		$ignoreFileids = array_map(fn ($dir) => $dir['parent'], $ignoreFiles);
+			->executeQuery();
+		/**
+		 * @var list<array> $ignoreFiles
+		 */
+		$ignoreFiles = $result->fetchAll();
+		$ignoreFileIds = array_map(fn ($dir): int => (int)$dir['parent'], $ignoreFiles);
 		foreach ($ignoreFiles as $ignoreFile) {
-			$ignoreDir = $this->getDir($ignoreFile['parent'], $directoryTypes, true);
-			$fileids = array_map(fn ($dir) => $dir['fileid'], $ignoreDir);
-			$ignoreFileids = array_merge($ignoreFileids, $fileids);
+			$ignoreDir = $this->getDir((int) $ignoreFile['parent'], $directoryTypes, true);
+			$fileIds = array_map(fn ($dir): int => (int) $dir['fileid'], $ignoreDir);
+			$ignoreFileIds = array_merge($ignoreFileIds, $fileIds);
 		}
-		return $ignoreFileids;
+		return $ignoreFileIds;
 	}
 }
