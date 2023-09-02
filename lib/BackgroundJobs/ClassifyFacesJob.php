ef823dcf31489481332d52c68f9dc0c46319f642
Marcel Klehr <mklehr@gmx.net>
2023-09-02 12:25:33 +0200

    enh: Strict types everywhere
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/BackgroundJobs/ClassifyFacesJob.php
+++ lib/BackgroundJobs/ClassifyFacesJob.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2022 The Recognize contributors.
  * This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
  */
-
+declare(strict_types=1);
 namespace OCA\Recognize\BackgroundJobs;
 
 use OCA\Recognize\Classifiers\Images\ClusteringFaceClassifier;
