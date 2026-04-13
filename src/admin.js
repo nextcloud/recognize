15c4812be5cd34357686b523a43fa4823ec8381a
Marcel Klehr <mklehr@gmx.net>
2026-04-13 13:46:03 +0200

    chore: Upgrade to vue 3 and nextcloud-vue 9
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/admin.js
+++ src/admin.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './components/ViewAdmin.vue'
 import AppGlobal from './mixins/AppGlobal.js'
 
-Vue.mixin(AppGlobal)
+const app = createApp(App)
+app.mixin(AppGlobal)
 
-global.Recognize = new Vue({
-	el: '#recognize',
-	render: h => h(App),
-})
+global.Recognize = app.mount('#recognize')
--- webpack.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-const webpackConfig = require('@nextcloud/webpack-vue-config')
-
-module.exports = webpackConfig
-webpackConfig.entry.admin = path.join(__dirname, 'src', 'admin.js')
